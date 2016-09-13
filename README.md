79011e331db9fbb7d9d6def1b4b59c6b60df18c6
Igor Kostenko <igor_kostenko@programmingresearch.com>
2016-09-13 12:10:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# so-39468373
+Stack Overflow 39468373 http://stackoverflow.com/questions/39468373/lowered-operations-in-roslyn
---
+++ so-39468373.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Semantics;
+using System.Linq;
+
+namespace so39468373
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var tree = CSharpSyntaxTree.ParseText(@"
+public class c
+{
+    public static c operator ++(c o) { return o; }
+    static c pre(c o) => ++o;
+    static c post(c o) => o++;
+    public static void Main() {}
+}");
+            var mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var compilation = CSharpCompilation.Create(null, new[] { tree }, new[] { mscorlib });
+            var model = compilation.GetSemanticModel(tree);
+            foreach (var node in tree.GetRoot().DescendantNodes().OfType<ArrowExpressionClauseSyntax>())
+            {
+                var operation = model.GetOperation(node);
+                var block = (IBlockStatement)operation;
+                var statement = (IReturnStatement)block.Statements.First();
+                var increment = (IIncrementExpression)statement.ReturnedValue;
+                // How to get lowered operations for increment here?
+            }
+        }
+    }
+}
